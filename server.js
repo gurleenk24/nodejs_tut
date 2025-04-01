f0658767caeea7faf0b452f6c87ad4791d572729
Gurleen Kaur <130543552+gurleenk24@users.noreply.github.com>
2025-04-01 18:46:24 +0000

    done

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("server file is running!!!!");
+// console.log("server file is running!!!!");
 
 // function add(a,b){
 //     return a+b;
@@ -10,14 +10,14 @@ console.log("server file is running!!!!");
 
 // var add = (a,b) => { return a+b;}
 
-var add = (a,b) => a+b;
+// var add = (a,b) => a+b;
 
-var ans = add(2,5);
-console.log(ans);
+// var ans = add(2,5);
+// console.log(ans);
 
-(function(){
-    console.log("Hello world");
-})();
+// (function(){
+//     console.log("Hello world");
+// })();
 
 // A callback function is a function that is passed 
 // as an argument to another function and is executed
@@ -26,3 +26,34 @@ console.log(ans);
 // are used for handling asynchronous operations, 
 // event listeners, and function execution control.
 
+
+function callback(){
+    console.log("Addition is successfully done!");
+}
+
+const add = function(a, b, callback){
+
+    process.stdout.write("Adding");
+
+    let dots = "";
+    let cnt = 0;
+
+    let interval = setInterval(()=>{
+        dots += ".";
+        process.stdout.write(".");
+        cnt++;
+
+        if(cnt === 3){
+           clearInterval(interval);
+
+            setTimeout(()=>{
+                let ans = a+b;
+                console.log("\nResult is : " + ans);
+                callback();
+            },500);
+        }
+   } ,500);
+
+};
+
+add(4,5,callback);
