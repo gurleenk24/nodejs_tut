a678e4c7d9ef17f882249646d558beabf5fb2b6a
Gurleen Kaur <130543552+gurleenk24@users.noreply.github.com>
2025-04-01 18:12:38 +0000

    commit

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 console.log("server file is running!!!!");
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// var add = function(a,b){
+//     return a+b;
+// }
+
+// var add = (a,b) => { return a+b;}
+
+var add = (a,b) => a+b;
+
+var ans = add(2,5);
+console.log(ans);
+
+(function(){
+    console.log("Hello world");
+})();
+
+// A callback function is a function that is passed 
+// as an argument to another function and is executed
+// later, usually after some operation completes.
+// In JavaScript (and many other languages), callbacks 
+// are used for handling asynchronous operations, 
+// event listeners, and function execution control.
+
