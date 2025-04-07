be7a0f363c3a472271ae3a1c5f734bb786ffdfbc
gurleen <gurleenkaurawal24@gmail.com>
2025-04-07 18:27:12 +0530

    db

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.get('/',(req,res) => {
-    res.send("How can I help you??");
+app.get('/chicken', (req, res) => {
+  res.send("How can I help you??");
+})
+
+app.get('/idli', (req, res)=>{
+  var customised_idli = {
+    name: 'rava idli',
+    size: '10 cm dia',
+    is_sambhar : true,
+    is_chutney: false
+  }
+  res.send(customised_idli);
+})
+
+app.post('/person',(req,res)=>{
+  res.send("data is saved");
 })
 
 app.listen(port, () => {
