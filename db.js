78ed3d578ff3baf0a2355615d7540288ad6617c7
gurleen <gurleenkaurawal24@gmail.com>
2025-04-09 00:38:57 +0530

    db

---
+++ db.js
@@ -0,0 +1,31 @@
+// this file will be responsible for database connection between server.js and mongoose 
+
+const mongoose = require('mongoose');
+
+// define the MongoDB connection URL
+const mongoURL = 'mongodb://localhost:27017/hotels';
+// connect to the MongoDB database
+mongoose.connect(mongoURL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+// get the default connection
+// Mongoose maintains a default connection object representing the connection to the database(mongoDB)
+const db = mongoose.connection;
+
+// defining event listeners
+db.on('connected', () => {
+    console.log('Connected to MongoDB server!');
+});
+db.on('error', (err) => {
+    console.error('MongoDB connection error!:', err);
+});
+db.on('disconnected', () => {
+    console.log('Disconnected from MongoDB server!');
+});
+
+// Export the database connection
+module.exports = db;
+
--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const db = require('./db');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to my Hotel!!!!!');
 })
 
-app.get('/chicken', (req, res) => {
-  res.send("How can I help you??");
-})
-
-app.get('/idli', (req, res)=>{
-  var customised_idli = {
-    name: 'rava idli',
-    size: '10 cm dia',
-    is_sambhar : true,
-    is_chutney: false
-  }
-  res.send(customised_idli);
-})
-
-app.post('/person',(req,res)=>{
-  res.send("data is saved");
-})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
